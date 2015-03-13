9c54a871dcc7a086307173f291cc598a287a043b
Aaron R. Muir <aaronmuir@gmail.com>
2015-03-13 00:15:44 -0500

    init solving class

---
+++ src/Solver.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+/**
+ * Aaron Muir
+ * CS 309
+ * LP Project
+ */
+public class Solver
+{
+    private ArrayList<Matrix> tables;
+    private ArrayList<AugRow> solutions;
+    private AugRow optimal;
+    private Method method;
+
+    private enum Method
+    {
+        simplex,
+        twoPhaseSimplex,
+        dualSimplex
+    }
+
+    /**
+     * LP solving algorithms
+     */
+    public Solver()
+    {
+        tables = new ArrayList<Matrix>();
+        solutions = new ArrayList<AugRow>();
+        optimal = new AugRow();
+        method = Method.simplex;
+    }
+
+    /**
+     * Returns the negation of the optimal solution's value.
+     * @return
+     */
+    public Double getObjValue()
+    {
+        return optimal.getB()*-1.0;
+    }
+
+
+}
