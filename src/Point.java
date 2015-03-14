b1da357ca6bc1d12f1617b295731a46a5e8ea2e9
Aaron R. Muir <aaronmuir@gmail.com>
2015-03-14 00:39:00 -0500

    point class to contain locations of key elements

---
+++ src/Point.java
@@ -0,0 +1,37 @@
+/**
+ * Aaron Muir
+ * CS 309
+ * LP Project
+ */
+public class Point
+{
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    private int x;
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    private int y;
+}
